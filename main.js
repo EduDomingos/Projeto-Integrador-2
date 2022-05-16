5bf83c68c9712be9f9f3b2322a1d5705bcef37fc
EduDomingos <88852683+EduDomingos@users.noreply.github.com>
2022-05-16 15:35:01 -0300

    Quinto dia Rocketseat Origin Final
    Projeto finalizado.

--- main.js
+++ main.js
@@ -5,6 +5,38 @@ onScroll()
 function onScroll() {
   showNavOnScroll()
   showBackToTopButtonOnScroll()
+
+  activateMenuAtCurrentSection(home)
+  activateMenuAtCurrentSection(services)
+  activateMenuAtCurrentSection(about)
+  activateMenuAtCurrentSection(contact)
+
+}
+
+function activateMenuAtCurrentSection(section) {
+  const targetLine = scrollY + innerHeight / 2
+
+  // verificar se section passou da targetLine
+
+  const sectionTop = section.offsetTop
+
+  const sectionHeight = section.offsetHeight
+
+  const sectionTopReachOrPassedTargetLine = targetLine >= sectionTop
+
+  const sectionEndsAt = sectionTop + sectionHeight
+
+  const sectionEndPassedTargetLine = sectionEndsAt <= targetLine
+
+  const sectionBounderies = sectionTopReachOrPassedTargetLine && !sectionEndPassedTargetLine
+
+  const sectionId = section.getAttribute('id')
+  const menuElement = document.querySelector(`.menu a[href*=${sectionId}]`)
+
+  menuElement.classList.remove('active')
+  if (sectionBounderies) {
+    menuElement.classList.add('active')
+  }
 }
 
 function showNavOnScroll() {
